317943c12d70bba312ccbf2c6474fcaaf3576324
ITSYORK\dmg530 <dmg530@york.ac.uk>
2021-03-09 15:49:05 +0000

    Initial Data Readings

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from helperFunctions import csvToArray
 
-
-path = '09-03-2021/13-52/data.csv'
+day = '09-03-2021'
+time = '14-46'
+file = 'ne0'
+path = day+'/'+time+'/'+file+'.csv'
 
 print(csvToArray(path))
+
