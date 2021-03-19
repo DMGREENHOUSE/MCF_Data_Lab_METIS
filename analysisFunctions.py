25efda4bc070d430581a5987dbcbbad4bb3e56e2
ITSYORK\dmg530 <dmg530@york.ac.uk>
2021-03-19 17:04:18 +0000

    Remainding files for records

--- analysisFunctions.py
+++ analysisFunctions.py
@@ -1,5 +1,11 @@
 def getTimeSpansForAnalysis():
-    timeRanges = [[45, 49],
+    timeRanges = [[43, 51],
                   [46, 48],
                   [46.5, 47.5]]
+    timeRanges = [[44, 45],
+                  [45.2, 46.2],
+                  [46.4, 47.4],
+                  [47.6, 48.6],
+                  [48.8, 49.8]]
+    #timeRanges = [[46, 48]]
     return timeRanges
